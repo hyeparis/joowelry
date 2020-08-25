10f472b479bbaace9485893a80489afbe812b8a3
hyeparis <jjubob93@gmail.com>
2020-08-24 19:57:23 -0700

    joowelry

---
+++ backend/data.js
@@ -0,0 +1,84 @@
+export default {
+    products: [
+        {
+        _id: '1',
+        name: 'Gold Rings',
+        image: '/img/img1.jpg',
+        price: 60,
+        mat: 'Gold',
+        rating: 4.5,
+        numReviews: 10,
+        countInStock: 0
+    },
+    {
+        _id: '2',
+        name: 'Silver Rings',
+        image: '/img/img2.jpg',
+        price: 40,
+        mat: 'Silver',
+        rating: 4.5,
+        numReviews: 10,
+        countInStock: 10
+    },
+    {
+        _id: '3',
+        name: 'DiamondRings',
+        image: '/img/img3.jpg',
+        price: 700000,
+        mat: 'Gold',
+        rating: 4.8,
+        numReviews: 10,
+        countInStock: 7
+    },
+    {
+        _id: '4',
+        name: 'Gold Rings',
+        image: '/img/img4.png',
+        price: 60,
+        mat: 'Gold',
+        rating: 4.5,
+        numReviews: 10,
+        countInStock: 8
+    },
+    {
+        _id: '5',
+        name: 'Gold Rings',
+        image: '/img/img5.jpg',
+        price: 60,
+        mat: 'Gold',
+        rating: 4.5,
+        numReviews: 10,
+        countInStock: 5
+    },
+    {
+        _id: '6',
+        name: 'Gold Rings',
+        image: '/img/img6.jpg',
+        price: 60,
+        mat: 'Gold',
+        rating: 4.5,
+        numReviews: 10,
+        countInStock: 3
+    },
+    {
+        _id: '7',
+        name: 'Gold Rings',
+        image: '/img/img5.jpg',
+        price: 60,
+        mat: 'Gold',
+        rating: 4.5,
+        numReviews: 10,
+        countInStock: 5
+    },
+    {
+        _id: '8',
+        name: 'Gold Rings',
+        image: '/img/img5.jpg',
+        price: 60,
+        mat: 'Gold',
+        rating: 4.5,
+        numReviews: 10,
+        countInStock: 5
+    }
+]
+}
